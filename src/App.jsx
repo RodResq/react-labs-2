a887e32aa1dee78afcda084c6107a234dc1ba9a7
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2025-08-05 15:18:50 -0300

    feat: state hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react'
 import './App.css'
 
-const App = (props) => {
-  const {counter} = props
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1)
+  , 1000)
+
+  console.log('rendereing...', counter);
+  
   return (
     <div>{counter}</div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,5 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-let counter = 1
+createRoot(document.getElementById('root')).render(<App />)
 
-const root = createRoot(document.getElementById('root'))
-
-const refresh = () => {
-  root.render(
-    <App counter={counter}/>
-  )
-}
-
-setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
