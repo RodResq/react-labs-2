a9552edf62a7f0f609689947143072564c5e9307
RodResq <rodrigoresque@yahoo.com.br>
2025-08-07 22:00:00 -0300

    feat: passing state to child component

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,26 @@ import { useState } from 'react'
 import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import { Button } from 'primereact/button';
 
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const ButtonExample = (props) => {
+  return (
+    <Button label={props.text} icon="pi pi-check" severity={props.type} onClick={props.onClick}/>
+  )
+}
+
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <PrimeReactProvider  value={{
@@ -12,8 +30,10 @@ const App = () => {
         locale: 'pt-BR'
     }}>
       <div>
-        <div>{counter}</div>
-        <Button label="Check" icon="pi pi-check" severity="success" onClick={() => setCounter(counter + 1)}/>
+        <Display  counter={counter} />
+        <ButtonExample text="plus" type="success" onClick={increaseByOne} />
+        <ButtonExample text="zero" type="warning" onClick={setToZero} />
+        <ButtonExample text="minus" type="danger" onClick={decreaseByOne} />
       </div>
     </PrimeReactProvider>
   )
