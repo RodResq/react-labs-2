7673e480194415df8e33c85910acb7b1a85e4c0b
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2025-08-20 13:02:12 -0300

    feat: event handle revised

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,19 @@ import { useState } from 'react'
 import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import { Button } from 'primereact/button';
 
-
-const Display = ({ counter }) => <div>{counter}</div>
-
-const ButtonExample = ({text, type, onClick }) => <Button label={text} icon="pi pi-check" severity={type} onClick={onClick}/>
-
-
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
 
-  const handleLefClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-  
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const handleClick = () => {
+    console.log('Clicked to Button')
+    setValue(0)
   }
     
   return (
-    <PrimeReactProvider  value={{
-        ripple: true,
-        inputStyle: 'outlined',
-        locale: 'pt-BR'
-    }}>
-      <div>
-        { left }
-        <ButtonExample text="left" type="success" onClick={handleLefClick} />
-        <ButtonExample text="right" type="danger" onClick={handleRightClick} />
-        { right }
-      </div>
-      <p>{allClicks.join(' ')}</p>
-    </PrimeReactProvider>
+    <div>
+      {value}
+      <button onClick={handleClick}>Button</button>
+    </div>
   )
 }
 
